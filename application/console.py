c82dec8dc5ac182842b1cccfb1b75cf756d1c4a1
Hironsan <light.tree.1.13@gmail.com>
2016-09-26 11:09:02 +0900

    update console version

--- application/console.py
+++ application/console.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from dialogue_system.bot import Bot
+import time
 
 
 if __name__ == '__main__':
     bot = Bot()
 
-    print('S: 料理のジャンルや場所をおっしゃってください。')
+    print('S: こんにちは!')
+    time.sleep(2)
+    print('S: わたくし舞子があなたの旅行先さがしをお手伝いさせていただきます。')
+    time.sleep(2)
+    print('S: どの辺りに行ってみたいとかありますか？')
     while True:
         sent = input('U: ')
         if sent == 'ありがとう':
